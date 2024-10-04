7e263d9abad1631699dc7fef507ca8a7fde2483f
Indah Syahfitri <146148856+ethereins@users.noreply.github.com>
2024-10-04 08:48:02 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'rumahjahit-cache-v1';
+const urlsToCache = [
+   '/',
+   '/index.html',
+   '/offline.html',
+   '/about.html',
+   '/contact.html',
+   '/styles.css', 
+   '/icons/icon-192x192.png',
+   '/icons/icon-512x512.png'
+];
+
+// Instalasi service worker dan caching halaman
+self.addEventListener('install', function(event) {
+   event.waitUntil(
+      caches.open(CACHE_NAME)
+      .then(function(cache) {
+         console.log('Opened cache');
+         return cache.addAll(urlsToCache);
+      })
+   );
+});
+
+self.addEventListener('fetch', function(event) {
+   event.respondWith(
+      caches.match(event.request)
+      .then(function(response) {
+         if (response) {
+            return response;
+         }
+         return fetch(event.request).catch(function() {
+            return caches.match('/offline.html');
+         });
+      })
+   );
+});
+
+self.addEventListener('activate', function(event) {
+   var cacheWhitelist = [CACHE_NAME];
+   event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+         return Promise.all(
+            cacheNames.map(function(cacheName) {
+               if (cacheWhitelist.indexOf(cacheName) === -1) {
+                  return caches.delete(cacheName);
+               }
+            })
+         );
+      })
+   );
+});
