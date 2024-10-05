f0a8149cdef6db172d4c1e5e59c96df4d4e7fccb
Indah Syahfitri <146148856+ethereins@users.noreply.github.com>
2024-10-05 15:58:37 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,54 @@
+const CACHE_NAME = 'rumah-jahit-bunda-cache';
 const urlsToCache = [
-   '/',
-   '/index.html',
-   '/offline.html',  // Pastikan offline.html dicache
-   '/about.html',
-   '/contact.html',
-   '/styles.css',
-   '/icons/icon-192x192.png',
-   '/icons/icon-512x512.png'
+  '/',
+  '/index.html',
+  '/offline.html',  
+  '/about.html',
+  '/contact.html',
+  '/styles.css'
 ];
+
 self.addEventListener('install', function(event) {
-   event.waitUntil(
-      caches.open(CACHE_NAME)
-      .then(function(cache) {
-         console.log('Opened cache');
-         return cache.addAll(urlsToCache);
-      })
-   );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-   event.respondWith(
-      caches.match(event.request)
-      .then(function(response) {
-         if (response) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(function() {
+        return caches.match('/offline.html').then(function(response) {
+          if (response) {
             return response;
-         }
-         return fetch(event.request).catch(function() {
-            return caches.match('offline.html');
-         });
-      })
-   );
+          } else {
+            self.clients.openWindow('/offline.html');
+          }
+        });
+      });
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
-   var cacheWhitelist = [CACHE_NAME];
-   event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-         return Promise.all(
-            cacheNames.map(function(cacheName) {
-               if (cacheWhitelist.indexOf(cacheName) === -1) {
-                  return caches.delete(cacheName);
-               }
-            })
-         );
-      })
-   );
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
