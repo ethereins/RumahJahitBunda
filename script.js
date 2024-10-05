de93e5824574c44cc79a2a7d0b5356f02328d2c4
Indah Syahfitri <146148856+ethereins@users.noreply.github.com>
2024-10-05 15:17:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ window.addEventListener('beforeinstallprompt', (e) => {
         });
     });
 });
+
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scroll ke bawah
+        navbar.style.top = "-80px"; // Menyembunyikan navbar (sesuaikan dengan tinggi navbar)
+    } else {
+        // Scroll ke atas
+        navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+});
+
