ad3a44cd5a68b5d118b36f972149350a848b3ae6
Indah Syahfitri <146148856+ethereins@users.noreply.github.com>
2024-10-06 09:29:06 +0700

    Add files via upload

---
+++ tugasdesainwebindahfix/service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'rumah-jahit-bunda-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/offline.html',  
+  '/about.html',
+  '/contact.html',
+  '/styles.css',
+  '/images/favicon1.png',
+  '/images/favicon2.png',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(function() {
+        return caches.match('/offline.html').then(function(response) {
+          if (response) {
+            return response;
+          } else {
+            self.clients.openWindow('/offline.html');
+          }
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
